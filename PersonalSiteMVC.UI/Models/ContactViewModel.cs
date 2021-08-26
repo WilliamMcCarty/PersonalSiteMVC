858ec07798b37d9b73a50b8d03963caf2cb5f0c0
William McCarty <kcchief@yahoo.com>
2021-08-26 16:26:21 -0500

    Added ContactViewModel under Models

---
+++ PersonalSiteMVC.UI/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteMVC.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Email Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Subject Required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Message Required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
