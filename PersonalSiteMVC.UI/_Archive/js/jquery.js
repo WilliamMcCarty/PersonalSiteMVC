568f539281ad4b3f098d8ef6b2d0a3d909082354
William McCarty <kcchief@yahoo.com>
2021-08-26 16:00:56 -0500

    Added The Archive Folder

---
+++ PersonalSiteMVC.UI/_Archive/js/jquery.js
@@ -0,0 +1,25 @@
+﻿
+jQuery(document).ready(function () {
+    console.log("jQuery is working!");
+});
+
+$(function () {
+    $(".thumb").on("click", function () {
+        var imgSrc = $(this).attr("src");
+        $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive-lightBox" />');
+        $("#lightbox-container").fadeIn(1300);
+        $("#lightbox-container").on("click", function () {
+            $(this).fadeOut(500);
+        });
+
+    });
+
+
+});
+
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true
+});
+
