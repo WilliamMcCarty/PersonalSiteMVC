1094e3b5673e76084c1f863f0922bd674a24869a
William McCarty <kcchief@yahoo.com>
2021-08-26 16:33:22 -0500

    HomeController Updated

--- PersonalSiteMVC.UI/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using PersonalSiteMVC.UI;
 using System.Net.Mail;
 using System.Configuration;
 using System.Net;
-
+using PersonalSiteMVC.UI.Models;
 
 
 namespace PersonalSiteMVC.UI.Controllers
@@ -39,6 +39,39 @@ namespace PersonalSiteMVC.UI.Controllers
             return View();
         }
 
-        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have received an email from {cvm.Email} with a subject: {cvm.Subject}.  Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+
+            MailMessage mm = new MailMessage(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailTo"].ToString(), cvm.Subject, message);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We are sorry, but your request could not be completed at this time. " +
+                    $"Please try again later.  Error Message: <br/> {ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmtation", cvm);
+        }
     }
 }
