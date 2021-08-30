91c8af8e8cac514b275d92d8f59421c2bd0ec9ea
William McCarty <kcchief@yahoo.com>
2021-08-30 02:02:51 -0500

    Fixed cotact form validation messages for formatting

--- PersonalSiteMVC.UI/Content/js/main.js
+++ PersonalSiteMVC.UI/Content/js/main.js
@@ -1,45 +1,45 @@
 ﻿//Copyright year
-let copyrightYear = document.getElementById('copyrightYear');
-copyrightYear.textContent = new Date().getFullYear();
+//let copyrightYear = document.getElementById('copyrightYear');
+//copyrightYear.textContent = new Date().getFullYear();
 
-function sendContactUs() {
-    let name = document.getElementById('tbName').value;
+//function sendContactUs() {
+//    let name = document.getElementById('tbName').value;
 
-    let email = document.getElementById('tbEmail').value;
+//    let email = document.getElementById('tbEmail').value;
 
-    let subject = document.getElementById("tbSubject").value;
+//    let subject = document.getElementById("tbSubject").value;
 
-    let message = document.getElementById('tbMessage').value;
+//    let message = document.getElementById('tbMessage').value;
 
-    let messageBack = document.getElementById('messageBack');
+//    let messageBack = document.getElementById('messageBack');
 
-    if (!name) {
-        let tbName = document.getElementById('tbName');
-        messageBack.style.color = 'darkred';
-        messageBack.textContent = "ERROR: Name Must Be Filled In";
-    }
-    else
-        if (!validateEmail(email)) {
-            messageBack.style.color = 'darkred';
-            messageBack.textContent = "ERROR: Email Must Be Filled In With Correct Format";
-        }
-        else
-            if (!subject) {
-                messageBack.style.color = 'darkred';
-                messageBack.textContent = "ERROR: Subject Must Be Filled In";
-            }
-            else
-                if (!message) {
-                    messageBack.style.color = 'darkred';
-                    messageBack.textContent = "ERROR: Message Must Be Filled In";
-                }
-                else {
-                    messageBack.style.color = 'darkgreen';
-                    messageBack.textContent = "Thank you for contacting me, I will respond as soon as possible";
-                }
-}
+//    if (!name) {
+//        let tbName = document.getElementById('tbName');
+//        messageBack.style.color = 'darkred';
+//        messageBack.textContent = "ERROR: Name Must Be Filled In";
+//    }
+//    else
+//        if (!validateEmail(email)) {
+//            messageBack.style.color = 'darkred';
+//            messageBack.textContent = "ERROR: Email Must Be Filled In With Correct Format";
+//        }
+//        else
+//            if (!subject) {
+//                messageBack.style.color = 'darkred';
+//                messageBack.textContent = "ERROR: Subject Must Be Filled In";
+//            }
+//            else
+//                if (!message) {
+//                    messageBack.style.color = 'darkred';
+//                    messageBack.textContent = "ERROR: Message Must Be Filled In";
+//                }
+//                else {
+//                    messageBack.style.color = 'darkgreen';
+//                    messageBack.textContent = "Thank you for contacting me, I will respond as soon as possible";
+//                }
+//}
 
-function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
+//function validateEmail(email) {
+//    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//    return re.test(email);
+//}
--- PersonalSiteMVC.UI/Models/ContactViewModel.cs
+++ PersonalSiteMVC.UI/Models/ContactViewModel.cs
@@ -9,16 +9,13 @@ namespace PersonalSiteMVC.UI.Models
     public class ContactViewModel
     {
         [Required(ErrorMessage = "* Name Required")]
-        [UIHint("MultilineText")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "* Email Required")]
-        [UIHint("MultilineText")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "* Subject Required")]
-        [UIHint("MultilineText")]
         public string Subject { get; set; }
 
         [Required(ErrorMessage = "* Message Required")]
